3921d7f7c290e0a586261c434e3d835e9ec15027
Gonzalo Ferreyra <gonferreyra@gmail.com>
2022-06-17 16:22:26 -0300

    deploy

--- js/card.js
+++ js/card.js
@@ -16,19 +16,21 @@ export const showCard = (searchId) => {
                     `
     result.appendChild(div);
 
-    // spinnerDiv.classList.remove('hidden')
+    /* Spinner
+    spinnerDiv.classList.remove('hidden')
 
-    // setTimeout(() => {
-    //     spinnerDiv.classList.add('hidden')
-    //     div.classList.add('result-container');
-    //     div.innerHTML = `
-    //                 <h2>${nombre}</h2>
-    //                 <p>Ingredientes: ${ingredientes}</p>
-    //                 <p>Precio: $${precio}</p>
-    //                 <img src='${img}'>
-    //                 `
-    //     result.appendChild(div);
-    // }, 2000);
+    setTimeout(() => {
+        spinnerDiv.classList.add('hidden')
+        div.classList.add('result-container');
+        div.innerHTML = `
+                    <h2>${nombre}</h2>
+                    <p>Ingredientes: ${ingredientes}</p>
+                    <p>Precio: $${precio}</p>
+                    <img src='${img}'>
+                    `
+        result.appendChild(div);
+    }, 2000);
+    */
 };
 
 export const cleanHTML = () => {
